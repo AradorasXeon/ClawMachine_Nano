9821f26dd770d03b7167255ed2645d621ec3fbb3
Krisztián József Varga <aradorasxeon@gmail.com>
2023-10-28 17:09:31 +0200

    1st working test version

--- ClawMachine_Nano.ino
+++ ClawMachine_Nano.ino
@@ -6,7 +6,7 @@
 #endif
 
 //comments here are my wire colors
-#define CLAW_PIN 2  //not colored
+#define CLAW_PIN 2  //not colored --> soldered directly to transistor
 #define LED_PIN 3   //dark green
 #define BUTTON 8    //yellow
 #define LEFT 9      //blue
@@ -14,16 +14,59 @@
 #define UP 11       //green
 #define DOWN 12     //white
 
+//nth LED, we can use these to light up appropaite LEDs for left, right, up, down movements
+#define LED_LEFT_MIN 0
+#define LED_LEFT_MAX 10
+#define LED_RIGHT_MIN 11
+#define LED_RIGHT_MAX 20
+#define LED_UP_MIN 21
+#define LED_UP_MAX 30
+#define LED_DOWN_MIN 31
+#define LED_DOWN_MAX 40
+
+#define LED_LAST 88
+#define LED_BRIGHTNESS 50
+
 
 // IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
 // pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
 // and minimize distance between Arduino and first pixel.  Avoid connecting
 // on a live circuit...if you must, connect GND first.
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(88, LED_PIN, NEO_GRB + NEO_KHZ800); // Constructor: number of LEDs, pin number, LED type
-Move msgMove; 
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(LED_LAST, LED_PIN, NEO_GRB + NEO_KHZ800); // Constructor: number of LEDs, pin number, LED type
+//88 LEDs @50 strength white about 700 mA
+//88 LEDs @80 strength white about 1070 mA
+//88 LEDs @100 strength white about 1290 mA
+//100 LEDs @100 strength white about 1310 mA
 
+Move msgMove(true);
 
+/// @brief Fills part of the actual Adafruit_NeoPixel led strip
+/// @param r red
+/// @param g green
+/// @param b blue
+/// @param startLed from this LED
+/// @param endLed to this LED
+void FillStripPart(uint8_t r, uint8_t g, uint8_t b, uint16_t startLed, uint16_t endLed)
+{
+
+  for(uint16_t i=startLed; i<=endLed; i++) 
+  {
+    strip.setPixelColor(i, strip.Color(r, g, b));
+    delay(1);
+  }
+
+  strip.show();
+}
+
+void LedClawAction()
+{
+  for(int i = 0; i<20; i++) //20 for test SHOULD TIME IT after claw touches down
+  {
+    FillStripPart(255, 0, 255, 0, LED_LAST);
+    delay(50);
+  }
+}
 
 const int TIME = 3500;
 
@@ -32,6 +75,7 @@ void setup()
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(CLAW_PIN, OUTPUT);
+  
   pinMode(BUTTON, INPUT);
   pinMode(LEFT, INPUT);
   pinMode(RIGHT, INPUT);
@@ -43,39 +87,90 @@ void setup()
 
 
   strip.begin();
-  strip.setBrightness(50);
+  strip.setBrightness(LED_BRIGHTNESS);
+  FillStripPart(1, 1, 1, 0, LED_LAST);
+
   strip.show(); // Initialize all pixels to 'off'
+}
+
+void MoveLeft()
+{
+  msgMove.setLeft();
+  FillStripPart(0, 255, 0, LED_LEFT_MIN, LED_LEFT_MAX);
+}
+
+void MoveRight()
+{
+  msgMove.setRight();
+  FillStripPart(255, 0, 0, LED_RIGHT_MIN, LED_RIGHT_MAX);
+
+}
+
+void MoveUp()
+{
+  msgMove.setUp();
+  FillStripPart(0, 0, 255, LED_UP_MIN, LED_UP_MAX);
+
+}
+
+void MoveDown()
+{
+  msgMove.setDown();
+  FillStripPart(0, 255, 255, LED_DOWN_MIN, LED_DOWN_MAX);
+}
+
+//stops inputs for a while
+void ClawAction()
+{
+  msgMove.setClawDown();
+  msgMove.sendMsg();
+  LedClawAction(); //contains delay have to send msg beforehand!
+  digitalWrite(CLAW_PIN, HIGH); //Closes claw
+  FillStripPart(255, 255, 0, 0, LED_LAST); //White light to see everything nice and clear
+  msgMove.setClawUp();
+  msgMove.sendMsg();
+  delay(2000); //will see the numbers
+  //will need some iteration
+  msgMove.setLeft();
+  msgMove.setDown();
+  //end of iteration
+  digitalWrite(CLAW_PIN, LOW); //Opens claw
 }
 
 void refreshButtonState()
 {
   //default zeroing:
-  msgMove.setDefaultValues();
-  if(digitalRead(BUTTON)) msgMove.setClaw();
-  if(digitalRead(LEFT)) msgMove.setLeft();
-  if(digitalRead(RIGHT)) msgMove.setRight();
-  if(digitalRead(UP)) msgMove.setUp();
-  if(digitalRead(DOWN)) msgMove.setDown();
-  msgMove.sendMsg();
-}
-
-
-// Fill the dots one after the other with a color
-void colorWipe(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
-    strip.setPixelColor(i, c);
-    strip.show();
-    //mondjuk ide írhatsz valamit, ami inputot figyeli
-    delay(wait);
+  if(digitalRead(BUTTON) == HIGH)
+  {
+    ClawAction();
   }
+   
+  if(digitalRead(LEFT) == HIGH)
+  {
+    MoveLeft();
+  }
+   
+  if(digitalRead(RIGHT) == HIGH)
+  {
+    MoveRight();
+  }
+   
+  if(digitalRead(UP) == HIGH)
+  {
+    MoveUp();
+  }
+   
+  if(digitalRead(DOWN) == HIGH)
+  {
+    MoveDown();
+  }
+   
 }
-
 
 void loop() 
 {
+  msgMove.setDefaultValues();
   refreshButtonState();
-  // Some example procedures showing how to display to the pixels:
-  colorWipe(strip.Color(255, 0, 0), 50); // Red
-  colorWipe(strip.Color(0, 255, 0), 50); // Green
-  colorWipe(strip.Color(0, 0, 255), 50); // Blue
+  msgMove.sendMsg();
+  FillStripPart(1, 1, 1, 0, LED_LAST);
 }
--- communication.h
+++ communication.h
@@ -1,9 +1,34 @@
+#include <Wire.h>
+
+#define COMMUNICATION_MOVEMENT 0u
+
+#define I2C_MOTOR_CTRL_ADDRESS 0xF2
+
+enum class X_Direction : uint8_t
+{
+    LEFT = 0,
+    IDLE_X = 127,
+    RIGHT = 255
+};
+
+enum class Y_Direction : uint8_t
+{
+    DOWN = 0,
+    IDLE_Y = 127,
+    UP = 255
+};
+
+enum class Claw_Direction : uint8_t
+{
+    CLAW_DOWN = 0,
+    IDLE_CLAW = 127,
+    CLAW_UP = 255
+};
+
 class Move
 {
     public:
-    uint8_t xAxis = 127; // 0 - Left, 255 - Right, any other number - do not move
-    uint8_t yAxis = 127; // 0 - "Down", 255 - "Up", any other number - do not move
-    bool initiateClaw = false; // true - initiate Claw movment, false - do nothing, after once set to true should be set back to false
+    Move(bool isMaster);
 
     void sendMsg();
     void setDefaultValues();
@@ -11,33 +36,117 @@ class Move
     void setRight();
     void setUp();
     void setDown();
-    void setClaw();
+    void setClawDown();
+    void setClawUp();
 
+    void readMsg(int byteCount);
+
+    X_Direction getX();
+    Y_Direction getY();
+    Claw_Direction getClaw();
+
+    static Move* instance; //for wire library hack
+
+    static void onReceiveCallBack(int byteCount);
 
     private:
-    uint8_t _x = 127;
-    uint8_t _y = 127;
-    bool claw = false;
+    X_Direction _x = X_Direction::IDLE_X;
+    Y_Direction _y = Y_Direction::IDLE_Y;
+    Claw_Direction _claw = Claw_Direction::IDLE_CLAW;
 };
 
+    ///@brief This is to hack the Wire.onReceive function, should be used ike: Wire.onReceive(Move::onReceiveCallBack); 
+    void Move::onReceiveCallBack(int byteCount)
+    {
+        if (instance != nullptr)
+        {
+            instance->readMsg(byteCount);
+        }
+    }
+
+
+    /// @brief ctor for the Move com interface
+    /// @param isMaster true if this is the i2c master
+    Move::Move(bool isMaster)
+    {
+        setDefaultValues();
+        if(isMaster)
+        {
+            Wire.begin();
+        }
+        else
+        {
+            Wire.begin(I2C_MOTOR_CTRL_ADDRESS);
+        }
+    }
+
+    /// @brief sends the msg on i2c
     void Move::sendMsg()
-    {}
+    {
+        Wire.beginTransmission(I2C_MOTOR_CTRL_ADDRESS);
+        Wire.write((uint8_t)_x);
+        Wire.write((uint8_t)_y);
+        Wire.write((uint8_t)_claw);
+        Wire.endTransmission(true);
+        delay(2);
+    }
+
+    void Move::readMsg(int byteCount)
+    {
+        _x = (X_Direction)Wire.read();
+        _y = (Y_Direction)Wire.read();
+        _claw = (Claw_Direction)Wire.read();
+    }
     
     void Move::setDefaultValues()
-    {}
+    {
+        _x = X_Direction::IDLE_X;
+        _y = Y_Direction::IDLE_Y;
+        _claw = Claw_Direction::IDLE_CLAW;
+    }
+
+    X_Direction Move::getX()
+    {
+        return _x;
+    }
+
+    Y_Direction Move::getY()
+    {
+        return _y;
+    }
+
+    Claw_Direction Move::getClaw()
+    {
+        return _claw;
+    }
+
 
     void Move::setLeft()
-    {}
+    {
+        _x = X_Direction::LEFT;
+    }
 
     void Move::setRight()
-    {}
+    {
+        _x = X_Direction::RIGHT;
+    }
 
     void Move::setUp()
-    {}
+    {
+        _y = Y_Direction::UP;
+    }
 
     void Move::setDown()
-    {}
+    {
+        _y = Y_Direction::DOWN;
+    }
 
-    void Move::setClaw()
-    {}
+    void Move::setClawDown()
+    {
+        _claw = Claw_Direction::CLAW_DOWN;
+    }
 
+    void Move::setClawUp()
+    {
+        _claw = Claw_Direction::CLAW_UP;
+    }
